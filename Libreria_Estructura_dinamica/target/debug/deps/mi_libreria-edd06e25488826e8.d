c9735fc64cd8d37dff545a7373bcde79bd083fab
luispinel123 <pinelp.luism.saj@gmail.com>
2026-05-13 11:00:37 -0400

    Delete Libreria_Estructura_dinamica/target/debug directory

--- Libreria_Estructura_dinamica/target/debug/deps/mi_libreria-edd06e25488826e8.d
+++
@@ -1,5 +0,0 @@
-D:\vscode\Libreria-Estructura-Dinamica-de-Datos-Rust-\Libreria_Estructura_dinamica\target\debug\deps\mi_libreria-edd06e25488826e8.d: src\main.rs
-
-D:\vscode\Libreria-Estructura-Dinamica-de-Datos-Rust-\Libreria_Estructura_dinamica\target\debug\deps\libmi_libreria-edd06e25488826e8.rmeta: src\main.rs
-
-src\main.rs:
--- Libreria_Estructura_dinamica/target/debug/deps/windows_link-f20c8d64b953bdfa.d
+++
@@ -1,6 +0,0 @@
-D:\vscode\Libreria-Estructura-Dinamica-de-Datos-Rust-\Libreria_Estructura_dinamica\target\debug\deps\windows_link-f20c8d64b953bdfa.d: C:\Users\UNITEC\ PC\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\windows-link-0.2.1\src\lib.rs C:\Users\UNITEC\ PC\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\windows-link-0.2.1\src\../readme.md
-
-D:\vscode\Libreria-Estructura-Dinamica-de-Datos-Rust-\Libreria_Estructura_dinamica\target\debug\deps\libwindows_link-f20c8d64b953bdfa.rmeta: C:\Users\UNITEC\ PC\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\windows-link-0.2.1\src\lib.rs C:\Users\UNITEC\ PC\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\windows-link-0.2.1\src\../readme.md
-
-C:\Users\UNITEC\ PC\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\windows-link-0.2.1\src\lib.rs:
-C:\Users\UNITEC\ PC\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\windows-link-0.2.1\src\../readme.md:
