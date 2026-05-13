d64c4c5bd70f007f6c20360f7617474145f6ae25
Adrian Monroy <monroy.adrian.saj@gmail.com>
2026-05-13 00:13:41 -0400

    Delete Libreria_Estructura_dinamica/lib.rs

--- Libreria_Estructura_dinamica/lib.rs
+++
@@ -1,7 +0,0 @@
-// Modulo principal de la libreria
-pub mod estructuras;
-pub mod helpers;
-
-// Re-exportar estructuras principales para facilitar el uso
-pub use estructuras::{Arbol, Cola, Pila, Grafo};
-pub use helpers::{read_file, write_file, Logger, LogLevel};
